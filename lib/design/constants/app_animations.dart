f6b7f06df278a225f31ca2100774e9c0ce89b452
Md-Sifatullah617 <sifatullah35-617@diu.edu.bd>
2023-10-10 13:27:12 +0600

    sdf

--- lib/design/constants/app_animations.dart
+++ lib/design/constants/app_animations.dart
@@ -1,3 +1,5 @@
 class AppAnimations {
   static const String introAnimation = 'assets/animations/intro_animation.riv';
+  static const String aboutAnimation =
+      'assets/animations/4283-8836-developer.riv';
 }
