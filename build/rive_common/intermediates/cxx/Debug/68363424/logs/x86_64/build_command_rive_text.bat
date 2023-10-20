652081301328be456eee52748e9465a04df95dd0
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-10-21 01:32:20 +0600

    del

--- build/rive_common/intermediates/cxx/Debug/68363424/logs/x86_64/build_command_rive_text.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\sifat\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\sifat\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.2.7\\android\\.cxx\\Debug\\68363424\\x86_64" ^
-  rive_text
