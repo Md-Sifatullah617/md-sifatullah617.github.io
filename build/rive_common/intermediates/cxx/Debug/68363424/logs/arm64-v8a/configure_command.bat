d9afae28827e25f2f6445bee6d82c45eff2fa5a0
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-10-21 00:00:29 +0600

    sdf

---
+++ build/rive_common/intermediates/cxx/Debug/68363424/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,18 @@
+@echo off
+"C:\\Users\\sifat\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\sifat\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.2.7\\android" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=19" ^
+  "-DANDROID_PLATFORM=android-19" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\sifat\\AppData\\Local\\Android\\sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\sifat\\AppData\\Local\\Android\\sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\sifat\\AppData\\Local\\Android\\sdk\\ndk\\25.1.8937393\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\sifat\\AppData\\Local\\Android\\sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\sifat\\Downloads\\Flutter project\\Projects\\md-sifatullah617.github.io\\build\\rive_common\\intermediates\\cxx\\Debug\\68363424\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\sifat\\Downloads\\Flutter project\\Projects\\md-sifatullah617.github.io\\build\\rive_common\\intermediates\\cxx\\Debug\\68363424\\obj\\arm64-v8a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\Users\\sifat\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\rive_common-0.2.7\\android\\.cxx\\Debug\\68363424\\arm64-v8a" ^
+  -GNinja
